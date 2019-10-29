c00d09a77fba6ca6ee02a6762a6a5c11ea8b5703
Mikkel Andreasen <andreasenmikkel1@gmail.com>
2019-10-29 10:55:21 +0100

    Update Forside

--- php/forside.php
+++ php/forside.php
@@ -4,13 +4,19 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="UTF-8">
-    <title>Front page</title>
-      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-        <style type="text/css">
-          body{ font: 14px sans-serif; }
-          .wrapper{ width: 350px; padding: 20px; }
-        </style>
-  </head>
+    <title>Login</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        body{ font: 20px sans-serif; }
+        .wrapper{ width: 500px; padding: 50px; }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Frontpage</h2>
+        <p>Here is all the fun!</p>
+    </div>
+</body>
 </html>
